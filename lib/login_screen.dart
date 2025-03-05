120d2e69a3b64dc9b9bcfdac5b881817b2526a7b
ilsong963 <ilsong963@naver.com>
2025-03-05 16:16:02 +0900

    카카오 로그인 및 로그인 버튼 배치

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,24 +1,63 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final db = FirebaseFirestore.instance;
-
     return Scaffold(
-        body: Column(
+        body: SafeArea(
+            child: Column(
       children: [
-        ElevatedButton(
-          onPressed: () {
-            GoRouter.of(context).push('/home');
-          },
-          child: Text('Login'),
-        ),
+        Spacer(),
+        Padding(
+            padding: EdgeInsets.all(30),
+            child: GestureDetector(
+              child: Image(image: AssetImage('assets/images/kakao_login_large_wide.png')),
+              onTap: () async {
+                await kakaoLogin(context);
+              },
+            )),
       ],
-    ));
+    )));
+  }
+
+  Future<void> kakaoLogin(BuildContext context) async {
+    if (await isKakaoTalkInstalled()) {
+      try {
+        await UserApi.instance.loginWithKakaoTalk();
+        log('카카오톡으로 로그인 성공');
+      } catch (error) {
+        log('카카오톡으로 로그인 실패 $error');
+
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+        }
+        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+        try {
+          await UserApi.instance.loginWithKakaoAccount();
+          GoRouter.of(context).push('/home');
+          log('카카오계정으로 로그인 성공');
+        } catch (error) {
+          log('카카오계정으로 로그인 실패 $error');
+        }
+      }
+    } else {
+      try {
+        await UserApi.instance.loginWithKakaoAccount();
+        GoRouter.of(context).push('/home');
+        log('카카오계정으로 로그인 성공');
+      } catch (error) {
+        log('카카오계정으로 로그인 실패 $error');
+      }
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:reservate_practice_room/route.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
+  KakaoSdk.init(nativeAppKey: '2b9e4a3cf1cf7014031990a186933f44');
   runApp(MyApp());
 }
 
